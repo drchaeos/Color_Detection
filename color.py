912da12e73ed3c4bd48a0c2ea2312f69004bef01
mkllel <vavfapi032@naver.com>
2023-11-09 15:13:16 +0900

    first commit

---
+++ color.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import pandas as pd
+import argparse
+
+#Creating argument parser to take image path from command line
+ap = argparse.ArgumentParser()
+# ap.add_argument('-i', '--image', required=True, help="Image Path")
+ap.add_argument('--image', default='/Users/min-kyulee/color_detection/colorpic.jpg', type=str, dest='image', required=False, help="Image Path")
+args = vars(ap.parse_args())
+img_path = args['image']
+
+#Reading the image with opencv
+img = cv2.imread(img_path)
+
+#declaring global variables (are used later on)
+clicked = False
+r = g = b = xpos = ypos = 0
+
+#Reading csv file with pandas and giving names to each column
+index=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('/Users/min-kyulee/color_detection/colors.csv', names=index, header=None)
+
+#function to calculate minimum distance from all colors and get the most matching color
+def getColorName(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+#function to get x,y coordinates of mouse double click
+def draw_function(event, x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+    
+cv2.destroyAllWindows()
